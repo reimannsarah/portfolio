b5442a1524f8031b70c30a50956d341a8265d78f
Sarah Reimann <reimannsarah45@gmail.com>
2023-08-02 15:55:25 -0700

    add header, background, style background divs with color, style header font

---
+++ src/components/Background.tsx
@@ -0,0 +1,9 @@
+export default function Background() {
+  return (
+    <>
+      <div className="circle1"></div>
+      <div className="circle2"></div>
+      <div className="circle3"></div>
+    </>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <div>
+        <div className="header-circle"></div>
+        <h1>Sarah Reimann</h1>
+      </div>
+      <div className="nav-btns">
+        <p>About</p>
+        <p>Projects</p>
+        <p>Contact</p>
+      </div>
+    </div>
+  )
+}
+
