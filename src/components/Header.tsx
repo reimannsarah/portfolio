e94fabedcc700081c88126280c77e4250d0f6f66
Sarah Reimann <reimannsarah45@gmail.com>
2023-08-02 22:07:42 -0700

    add onclick functions to buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,23 @@
 interface HeaderProps {
+  onHomeClick: () => void;
   onAboutClick: () => void;
+  onProjectsClick: () => void;
+  onContactClick: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ onAboutClick}) => {
+const Header: React.FC<HeaderProps> = ({ onHomeClick, onAboutClick, onProjectsClick, onContactClick}) => {
 
   
   return (
     <div className="header">
       <div>
         <div className="header-circle"></div>
-        <h1>Sarah Reimann</h1>
+        <h1 onClick={onHomeClick}>Sarah Reimann</h1>
       </div>
       <div className="nav-btns">
         <p className="about-btn" onClick={onAboutClick}>About</p>
-        <p className="projects-btn">Projects</p>
-        <p className="contact-btn">Contact</p>
+        <p className="projects-btn" onClick={onProjectsClick}>Projects</p>
+        <p className="contact-btn" onClick={onContactClick}>Contact</p>
       </div>
     </div>
   )
