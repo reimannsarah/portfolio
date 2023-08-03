4537ae28fcb897fb45707744092e3189716edf52
Sarah Reimann <reimannsarah45@gmail.com>
2023-08-02 22:08:15 -0700

    add home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return (
+    <div className="bio">
+      <p>full stack developer. oregon native. colorado college grad. loves running for fun, making art, and fresh pasta.</p>
+    </div>
+  )
+}
+
+export default Home;
