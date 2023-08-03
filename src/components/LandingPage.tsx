b25f6ab39aa4cd520b8c55c6c4805c5bbef50cbf
Sarah Reimann <reimannsarah45@gmail.com>
2023-08-03 15:19:33 -0700

    update landing page title and add description

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -7,6 +7,7 @@ const LandingPage: React.FC = () => {
       <img className="land-pg-background" src={background} alt="abstract painting"/>
       <div className="lp-title">
       <h1 >sarah reimann</h1>
+      <p className="lp-description">full stack web developer</p>
       <Link className="arrow" to="/home">&#8594;</Link>
       </div>
     </div>
