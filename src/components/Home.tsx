d7c3326122638ed5598ceaadb558014441247af3
Sarah Reimann <reimannsarah45@gmail.com>
2023-08-02 23:47:21 -0700

    add profile picture

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,10 @@
+import sarah from '../assets/imgs/sarah.png';
+
 const Home: React.FC = () => {
   return (
     <div className="bio">
-      <p>full stack developer. oregon native. colorado college grad. loves running for fun, making art, and fresh pasta.</p>
+      <img className="port-pic" src={sarah} alt="a photo of sarah standing under a plant"/>
+      <p className="intro">full stack developer. oregon native. colorado college grad. loves running for fun, making art, and fresh pasta.</p>
     </div>
   )
 }
