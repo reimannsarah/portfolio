6f91aba92b09afc89d41f2ceee5010a028ebb9b4
Sarah Reimann <reimannsarah45@gmail.com>
2023-08-03 00:14:46 -0700

    update profile picture and background div

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,11 @@ import sarah from '../assets/imgs/sarah.png';
 
 const Home: React.FC = () => {
   return (
-    <div className="bio">
-      <img className="port-pic" src={sarah} alt="a photo of sarah standing under a plant"/>
+    <div>
+      <div className="pic-div">
+        <img className="port-pic" src={sarah} alt="a photo of sarah standing under a plant"/>
+        <div className="pic-arch"></div>
+      </div>
       <p className="intro">full stack developer. oregon native. colorado college grad. loves running for fun, making art, and fresh pasta.</p>
     </div>
   )
