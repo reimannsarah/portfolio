f52ad1f2c786462f31a89341a6d30b7a7a76a336
Sarah Reimann <reimannsarah45@gmail.com>
2023-08-07 20:58:32 -0700

    add projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,27 @@
-const Projects: React.FC = () => {
+interface Project {
+  name: string;
+  src: string;
+  description: string;
+  url: string;
+}
+
+interface ProjectsProps {
+  projects: Project[];
+}
+
+const Projects: React.FC<ProjectsProps> = ({ projects }) => {
   return (
-    <div>
-      <p>Projects</p>
+    <div className="project-cards">
+      {projects.map(project => {
+        return (
+        <div className="card">
+          <a href={project.url} className="host-link" target="_blank"/>
+          <img src={project.src}/>
+          <h1 className="proj-title">{project.name}</h1>
+          <p>{project.description}</p>
+        </div>
+        )
+      })}
     </div>
   )
 }
