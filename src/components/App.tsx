bf6db854eead1d839b99950c09bf09518dc84b47
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-14 15:35:49 -0800

    redesign app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,19 @@
 import './App.css'
-import React, { ReactNode } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Control from './Control';
-import LandingPage from './LandingPage';
-
-interface MainLayoutProps {
-  children: ReactNode;
-}
-
-const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-  return (
-    <React.Fragment>
-      <div className="circle1"></div>
-      <div className="circle2"></div>
-      <div className="circle3"></div>
-      {children}
-    </React.Fragment>
-  )
-}
+// import React, { ReactNode } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Control from './Control';
+// import LandingPage from './LandingPage';
+// import Home from './Home';
+// import Contact from './Contact';
+import Header from './Header';
+// import Projects from './Projects';
 
 function App() {
 
   return (
-  <Router>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/home" element={<MainLayout><Control /></MainLayout>} />
-    </Routes>
-  </Router>
+    <div className='app'>
+      <Header />
+    </div>
   )
 }
 
