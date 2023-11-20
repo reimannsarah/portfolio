9cb2efd2cff21bd11e6a6b5ec07d6fe87fe332b6
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-20 13:23:07 -0800

    WIP

--- src/components/Control.tsx
+++ src/components/Control.tsx
@@ -1,70 +1,70 @@
-import { useState } from 'react';
-import React from 'react';
-import Header from './molecules/Header';
-import Home from './Home';
-import About from './About';
-import Projects from './Projects';
-import Contact from './Contact';
-import projectList from './store';
+// import { useState } from 'react';
+// import React from 'react';
+// import Header from './molecules/Header';
+// import Home from './Home';
+// import About from './About';
+// import Projects from './Projects';
+// import Contact from './Contact';
+// import projectList from './store';
 
-const Control: React.FC = () => {
+// const Control: React.FC = () => {
 
-  const [homeVisible, setHomeVisible] = useState(true);
-  const [aboutVisible, setAboutVisible] = useState<boolean>(false);
-  const [projectsVisible, setProjectsVisible] = useState<boolean>(false);
-  const [contactVisible, setContactVisible] = useState<boolean>(false);
-  // const [visibleProjectIndex, setVisibleProjectIndex] = useState(0);
+//   const [homeVisible, setHomeVisible] = useState(true);
+//   const [aboutVisible, setAboutVisible] = useState<boolean>(false);
+//   const [projectsVisible, setProjectsVisible] = useState<boolean>(false);
+//   const [contactVisible, setContactVisible] = useState<boolean>(false);
+//   // const [visibleProjectIndex, setVisibleProjectIndex] = useState(0);
 
-  const handleHomeClick = () => {
-    setHomeVisible(true);
-    setAboutVisible(false);
-    setProjectsVisible(false);
-    setContactVisible(false);
-  }
+//   const handleHomeClick = () => {
+//     setHomeVisible(true);
+//     setAboutVisible(false);
+//     setProjectsVisible(false);
+//     setContactVisible(false);
+//   }
 
-  const handleAboutClick = () => {
-    setAboutVisible(true);
-    setHomeVisible(false);
-    setProjectsVisible(false);
-    setContactVisible(false);
-  }
+//   const handleAboutClick = () => {
+//     setAboutVisible(true);
+//     setHomeVisible(false);
+//     setProjectsVisible(false);
+//     setContactVisible(false);
+//   }
 
-  const handleProjectsClick = () => {
-    setProjectsVisible(true);
-    setHomeVisible(false);
-    setAboutVisible(false);
-    setContactVisible(false);
-  }
+//   const handleProjectsClick = () => {
+//     setProjectsVisible(true);
+//     setHomeVisible(false);
+//     setAboutVisible(false);
+//     setContactVisible(false);
+//   }
 
-  const handleContactClick = () => {
-    setContactVisible(true);
-    setHomeVisible(false);
-    setProjectsVisible(false);
-    setAboutVisible(false);
-  }
+//   const handleContactClick = () => {
+//     setContactVisible(true);
+//     setHomeVisible(false);
+//     setProjectsVisible(false);
+//     setAboutVisible(false);
+//   }
 
-  let currentlyVisible = null;
+//   let currentlyVisible = null;
 
-    if (homeVisible) {
-      currentlyVisible = 
-      <Home />
-    } else if (aboutVisible) {
-      currentlyVisible = 
-      <About />
-    } else if (projectsVisible) {
-      currentlyVisible = 
-        <Projects projects={projectList}/>
-    } else if (contactVisible) {
-      currentlyVisible = 
-      <Contact />
-    }
+//     if (homeVisible) {
+//       currentlyVisible = 
+//       <Home />
+//     } else if (aboutVisible) {
+//       currentlyVisible = 
+//       <About />
+//     } else if (projectsVisible) {
+//       currentlyVisible = 
+//         <Projects projects={projectList}/>
+//     } else if (contactVisible) {
+//       currentlyVisible = 
+//       <Contact />
+//     }
   
-    return (
-      <React.Fragment>
-        <Header onHomeClick={handleHomeClick} onAboutClick={handleAboutClick} onProjectsClick={handleProjectsClick} onContactClick={handleContactClick}/>
-        {currentlyVisible} 
-      </React.Fragment>
-    )
-}
+//     return (
+//       <React.Fragment>
+//         <Header onHomeClick={handleHomeClick} onAboutClick={handleAboutClick} onProjectsClick={handleProjectsClick} onContactClick={handleContactClick}/>
+//         {currentlyVisible} 
+//       </React.Fragment>
+//     )
+// }
 
-export default Control;
+// export default Control;
