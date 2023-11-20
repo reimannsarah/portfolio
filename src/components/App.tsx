6b75c34d63f72ccad542c955068a3ce571cfeef4
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-20 12:20:58 -0800

    remove unnecessary imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,13 @@
 import './App.css'
-// import React, { ReactNode } from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Control from './Control';
-// import LandingPage from './LandingPage';
-// import Home from './Home';
-// import Contact from './Contact';
+import EduExp from './molecules/EduExp';
 import Header from './molecules/Header';
-// import Projects from './Projects';
 
 function App() {
 
   return (
     <div className='app'>
       <Header />
+      <EduExp />
     </div>
   )
 }
