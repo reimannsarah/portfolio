e18dc401b11ddb9e1c155c540faf8fa81068361c
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-20 21:58:51 -0800

    add central file for all page text

---
+++ src/assets/text/pageStrings.ts
@@ -0,0 +1,10 @@
+const pageStrings = {
+  header: "sarah reimann",
+  education_subheader: "education",
+  education: ["school", "more schoole"],
+  experience_subheader: "experience",
+  experience: ["work experience", "more work experience"],
+  skills_subheader: "skills",
+}
+
+export default pageStrings;
