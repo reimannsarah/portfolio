c0ccd27f5f5e2017d9fe8e1c91e00f083bd7c4e8
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-22 01:05:01 -0800

    create footer component

---
+++ src/components/molecules/Footer.tsx
@@ -0,0 +1,12 @@
+import logo from '../../assets/imgs/logo_name.png'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>&#xa9; Sarah Reimann</p>
+      <img src={logo} alt="logo"/>
+    </div>
+  )
+}
+
+export default Footer;
