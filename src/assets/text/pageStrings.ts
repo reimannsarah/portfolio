3d23ed344bf5fcf71c10d7d87572a46cdd2c0199
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-22 00:18:25 -0800

    update info in education and experience

--- src/assets/text/pageStrings.ts
+++ src/assets/text/pageStrings.ts
@@ -1,9 +1,35 @@
 const pageStrings = {
   header: "sarah reimann",
   education_subheader: "education",
-  education: ["school", "more schoole"],
+  education: [
+    {
+      school: "Epicodus",
+      dates: "March 2023 - September 2023",
+      degree: "Certificate in Full Stack Development",
+      location: "Portland, OR"
+    },
+    {
+      school: "Colorado College",
+      dates: "September 2015 - May 2019",
+      degree: "Bachelor of Arts in Political Science",
+      location: "Colorado Springs, CO"
+    }
+  ],
   experience_subheader: "experience",
-  experience: ["work experience", "more work experience"],
+  experience: [
+    {
+      company: "Self Employed",
+      one: "point 1",
+      two: "point 2",
+      three: "point 3"
+    },
+    {
+      company: "Olio",
+      one: "point 1",
+      two: "point 2",
+      three: "point 3"
+    }
+  ],
   skills_subheader: "skills",
 }
 
