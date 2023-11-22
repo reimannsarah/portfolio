9860cf922e85e6b2d352c80251c27b302b03b16f
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-22 00:37:16 -0800

    add animation to cycle through skills array

--- src/components/molecules/Skills.tsx
+++ src/components/molecules/Skills.tsx
@@ -1,10 +1,25 @@
 import pageStrings from "../../assets/text/pageStrings";
+import {useState, useEffect} from "react";
 
 const Skills = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % pageStrings.skills.length);
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
     <div className="skills">
       <p>{pageStrings.skills_subheader}</p>
-      <div className="skills-circle"></div>
+      <div className="skills-circle">
+        <p>{pageStrings.skills[currentIndex].name}</p>
+      </div>
     </div>
   )
 }
