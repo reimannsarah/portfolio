59fcc61a4f2811a88c8dc912e66ca035f6c7f5ae
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-27 23:42:26 -0800

    move projects file into molecules folder

---
+++ src/components/molecules/Projects.tsx
@@ -0,0 +1,34 @@
+interface Project {
+  name: string;
+  src: string;
+  description: string;
+  techStack: string;
+  url: string;
+}
+
+interface ProjectsProps {
+  projects: Project[];
+}
+
+const Projects: React.FC<ProjectsProps> = ({ projects }) => {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <div className="project-cards">
+        {projects.map((project, index) => {
+          return (
+          <div className="card" key={index}>
+            <a href={project.url} className="host-link" target="_blank"/>
+            <img src={project.src}/>
+            <h1 className="proj-title">{project.name}</h1>
+            <p>{project.description}</p>
+            <p>{project.techStack}</p>
+          </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Projects;
