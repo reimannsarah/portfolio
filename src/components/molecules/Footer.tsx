39615f01c22ad95f8a7b33d219210048d16d642f
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-30 00:33:00 -0800

    update to use pagestrings

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -1,9 +1,10 @@
-import logo from '../../assets/imgs/logo_name.png'
+import logo from '../../assets/imgs/logo_name.png';
+import pageStrings from '../../assets/text/pageStrings';
 
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>&#xa9; Sarah Reimann</p>
+      <p>{pageStrings.copyright}</p>
       <div className='logo-icons'>
         <div className='icons'>
           <div className='icon'></div>
