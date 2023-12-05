955f027f03703df4cfe0563bd3b7d4c19bf732e0
Sarah Reimann <reimannsarah45@gmail.com>
2023-12-05 15:59:58 -0800

    move store info to pagestrings

--- src/assets/text/pageStrings.ts
+++ src/assets/text/pageStrings.ts
@@ -56,6 +56,71 @@ const pageStrings = {
     }
   ],
   projects_subheader: "projects",
+  projects: [
+    {
+      name: 'Mad Libs',
+      src: imageUrls.mad_libs,
+      description: "App that allows users to log in to their account, create mad libs and fill out available mad libs. Quizzes and results are saved to a Firestore Database and listed on the home page. Built with React using Firebase/Firestore",
+      techStack: "JavaScript | JSX | CSS | | React | Firebase/Firestore | Webpack | Node.js",
+      url: "https://github.com/reimannsarah/mad-libs.git"
+    },
+  
+    {
+      name: "Birthday RSVP",
+      src: imageUrls.bday_rsvp,
+      description: "Webpage that allows the  user to input an amount of free time and desired activities or chores. The user can then use the app to budget their free time efficiently. Built with JavaScript and styled with CSS.",
+      techStack: "React | Javascript | JSX | webpack | Node.js | CSS | MySQL",
+      url: "https://sarah-reimann-birthday.web.app/"
+    },
+  
+    {
+      name: "Parenting Consulting Website",
+      src: imageUrls.pc_website,
+      description: "A personal website for a parenting consulting business. Built with Typescript and  React.",
+      techStack: "Typescript | TSX | Javascript | Vite | Node.js | CSS | Firebase",
+      url: "https://lucy-grein-pc.web.app/"
+    },
+  
+    {
+      name: "Mouse Mingle",
+      src: imageUrls.mouse_mingle,
+      description: "Group project from Epicodus to practice C#. This is a dating website for rodents. There is an API that makes use of a MySQL database. A use can create a profile and save their data to the database. There is a client side with a front end also built with C#.",
+      techStack: "C# | .NET | ASP.NET Core | MVC | Entity Framework Core | EF Core Migrations | Swashbuckle | Swagger | MySQL | RestSharp",
+      url: "https://github.com/reimannsarah/MouseMingleMonoRepo.git"
+    },
+  
+    {
+      name: "Prioritime",
+      src: imageUrls.prioritime,
+      description: "Group project from Epicodus. This is an app where you can enter the amount of free time you have on a given day and then split that up into 15 minute blocks and assign the time to various activities.",
+      techStack: "HTML | CSS | Javascript | Webpack | npm",
+      url: "https://github.com/reimannsarah/prioritime.git"
+    },
+  
+    {
+      name: "Currency Exchanger",
+      src: imageUrls.currency,
+      description: "An application where a user can exchange currency rates between 160 different currencies.",
+      techStack: "HTML | Javascript | CSS | Webpack | npm",
+      url: "https://github.com/reimannsarah/currency-exchanger.git"
+    },
+  
+    {
+      name: "Studio RJR Portfolio",
+      src: imageUrls.studio_rjr,
+      description: "A portfolio website for a local artist. User can browse art and contact artist",
+      techStack: "Typescript | TSX | Javascript | Vite | Node.js | CSS | Firebase",
+      url: "https://github.com/reimannsarah/currency-exchanger.git"
+    },
+  
+    {
+      name: "Super Galactic Age Calculator",
+      src: imageUrls.galactic,
+      description: "Calculate your age and the time between your current age and a past or future age depending on the solar years of a few different planets.",
+      techStack: "Javascript | HTML | CSS Webpack | npm",
+      url: "https://github.com/reimannsarah/super-galactic-age-calculator.git"
+    }
+  ],
   about: {
       subheader: "about",
       p1: "My name is Sarah Reimann, I'm 27 years old and I grew up in the southwest hills of Portland, OR. After finishing highschool at St.Mary's Academy, I went to Colorado College where I got my bachelor's degree in Political Science. I loved learning about the structures of government all around the world and thinking about how the systems can be improved. I especially loved living in the mountains in Colorado and spending my time hiking, skiing, and visiting my family in Denver.",
