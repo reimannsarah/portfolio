b85199f005f0270f420626f97b97e8187d965e31
Sarah Reimann <reimannsarah45@gmail.com>
2023-12-05 15:29:34 -0800

    add imageUrls object

---
+++ src/assets/imgs/imageURLs.ts
@@ -0,0 +1,12 @@
+const imageUrls = {
+  logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/logo.png',
+  hero: 'https://srportfolio.blob.core.windows.net/portfolio-images/art1.png',
+  js_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/js_logo.png',
+  csharp_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/csharp_logo_.png',
+  css_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/css_logo.png',
+  dotnet_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/dot_net_logo.png',
+  react_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/react_logo.png',
+  ts_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/ts_logo.png'
+}
+
+export default imageUrls;
