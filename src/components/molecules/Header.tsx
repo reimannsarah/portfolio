e7748aab20500bff052b8a96344c77291254f119
Sarah Reimann <reimannsarah45@gmail.com>
2023-12-05 15:30:51 -0800

    use imageUrls

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,16 +1,16 @@
-// import logo from '../../assets/imgs/logo-white-bgc.png'
 import useBlobImage from '../../services/useBlobImage';
 import pageStrings from '../../assets/text/pageStrings';
+import imageUrls from '../../assets/imgs/imageURLs';
 
 const Header: React.FC = () => {
-  const imageURL = 'https://srportfolio.blob.core.windows.net/portfolio-images/logo.png';
-  const imageSrc = useBlobImage(imageURL);
+  const imageURL = imageUrls.logo;
+  const [imageSrc] = useBlobImage(imageURL);
   
   
   return (
     <div className='header'>
       <div className='logo'>
-        <img src={imageSrc} alt="logo"/>
+        {imageSrc && <img src={imageSrc} alt="logo" />}
       </div>
         <p className='header-name'>{pageStrings.header}</p>
     </div>
