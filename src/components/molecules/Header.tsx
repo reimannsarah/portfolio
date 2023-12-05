c0981aedaa6a044157711b7e18eeb99d77540e7a
Sarah Reimann <reimannsarah45@gmail.com>
2023-12-04 23:11:01 -0800

    WIP accessing blob

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,13 +1,16 @@
-import logo from '../../assets/imgs/logo-white-bgc.png'
+// import logo from '../../assets/imgs/logo-white-bgc.png'
+import useBlobImage from '../../services/useBlobImage';
 import pageStrings from '../../assets/text/pageStrings';
 
 const Header: React.FC = () => {
-
+  const imageURL = 'https://srportfolio.blob.core.windows.net/portfolio-images/logo.png';
+  const imageSrc = useBlobImage(imageURL);
+  
   
   return (
     <div className='header'>
       <div className='logo'>
-        <img src={logo} alt="logo"/>
+        <img src={imageSrc} alt="logo"/>
       </div>
         <p className='header-name'>{pageStrings.header}</p>
     </div>
