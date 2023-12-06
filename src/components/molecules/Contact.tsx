f710728ccebe861ed37043670af30be167120e38
Sarah Reimann <reimannsarah45@gmail.com>
2023-12-06 00:42:08 -0800

    move file

---
+++ src/components/molecules/Contact.tsx
@@ -0,0 +1,17 @@
+const Contact: React.FC = () => {
+  return (
+    <div className="contact">
+      <form action="https://api.web3forms.com/submit" method="POST">
+        <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" />
+        <input type="text" name="name" required />
+        <input type="email" name="email" required />
+        <textarea name="message" required></textarea>
+        <div className="h-captcha" data-captcha="true"></div>
+        <button type="submit">Submit Form</button>
+      </form>
+      <script src="https://web3forms.com/client/script.js" async defer></script>
+    </div>
+  )
+}
+
+export default Contact;
