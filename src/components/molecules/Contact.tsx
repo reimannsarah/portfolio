1d0f6963ee0064f74f9d268c3286f089e555ce6a
Sarah Reimann <reimannsarah45@gmail.com>
2023-12-08 15:23:46 -0800

    put return imside div

--- src/components/molecules/Contact.tsx
+++ src/components/molecules/Contact.tsx
@@ -19,15 +19,17 @@ const Contact: React.FC = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className='contact-form'>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
   );
 }
 
