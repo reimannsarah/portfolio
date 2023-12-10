1cb6a8f362bd44c301b2d84f45fb3c858beac2bd
Sarah Reimann <reimannsarah45@gmail.com>
2023-12-10 01:14:13 -0800

    categorize links

--- src/assets/imgs/imageURLs.ts
+++ src/assets/imgs/imageURLs.ts
@@ -1,26 +1,36 @@
 const imageUrls = {
-  logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/logo-white-bgc.png',
-  logo_name: 'https://srportfolio.blob.core.windows.net/portfolio-images/logo_name.png',
-  logo_linen_bgc: 'https://srportfolio.blob.core.windows.net/portfolio-images/logo.png',
-  hero: 'https://srportfolio.blob.core.windows.net/portfolio-images/art1.png',
-  middle_art1: 'https://srportfolio.blob.core.windows.net/portfolio-images/art2.png',
-  middle_art2: 'https://srportfolio.blob.core.windows.net/portfolio-images/art3.png',
-  js_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/js_logo.png',
-  csharp_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/csharp_logo_.png',
-  css_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/css_logo.png',
-  dotnet_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/dot_net_logo.png',
-  react_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/react_logo.png',
-  ts_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/ts_logo.png',
-  mad_libs: 'https://srportfolio.blob.core.windows.net/portfolio-images/mad-libs.png',
-  bday_rsvp: 'https://srportfolio.blob.core.windows.net/portfolio-images/bday-RSVP.png',
-  pc_website: 'https://srportfolio.blob.core.windows.net/portfolio-images/pc-website.png',
-  mouse_mingle: 'https://srportfolio.blob.core.windows.net/portfolio-images/mouse-mingle.png',
-  prioritime: 'https://srportfolio.blob.core.windows.net/portfolio-images/prioritime.png',
-  currency: 'https://srportfolio.blob.core.windows.net/portfolio-images/currency.png',
-  studio_rjr: 'https://srportfolio.blob.core.windows.net/portfolio-images/studio_rjr.png',
-  galactic: 'https://srportfolio.blob.core.windows.net/portfolio-images/galactic.png',
-  sarah_photo: 'https://srportfolio.blob.core.windows.net/portfolio-images/sarah2.jpg',
-  bottom_art: 'https://srportfolio.blob.core.windows.net/portfolio-images/art4.png'
+  logos: {
+    logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/logo-white-bgc.png',
+    logo_name: 'https://srportfolio.blob.core.windows.net/portfolio-images/logo_name.png',
+    logo_linen_bgc: 'https://srportfolio.blob.core.windows.net/portfolio-images/logo.png',
+  },
+  art: {
+    hero: 'https://srportfolio.blob.core.windows.net/portfolio-images/art1.png',
+    middle_art1: 'https://srportfolio.blob.core.windows.net/portfolio-images/art2.png',
+    middle_art2: 'https://srportfolio.blob.core.windows.net/portfolio-images/art3.png',
+    bottom_art: 'https://srportfolio.blob.core.windows.net/portfolio-images/art4.png'
+  },
+  skills_logos: {
+    js_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/js_logo.png',
+    csharp_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/csharp_logo_.png',
+    css_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/css_logo.png',
+    dotnet_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/dot_net_logo.png',
+    react_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/react_logo.png',
+    ts_logo: 'https://srportfolio.blob.core.windows.net/portfolio-images/ts_logo.png'
+  },
+  projects: {
+    mad_libs: 'https://srportfolio.blob.core.windows.net/portfolio-images/mad-libs.png',
+    bday_rsvp: 'https://srportfolio.blob.core.windows.net/portfolio-images/bday-RSVP.png',
+    pc_website: 'https://srportfolio.blob.core.windows.net/portfolio-images/pc-website.png',
+    mouse_mingle: 'https://srportfolio.blob.core.windows.net/portfolio-images/mouse-mingle.png',
+    prioritime: 'https://srportfolio.blob.core.windows.net/portfolio-images/prioritime.png',
+    currency: 'https://srportfolio.blob.core.windows.net/portfolio-images/currency.png',
+    studio_rjr: 'https://srportfolio.blob.core.windows.net/portfolio-images/studio_rjr.png',
+    galactic: 'https://srportfolio.blob.core.windows.net/portfolio-images/galactic.png',
+  },
+  photos: {
+  sarah_photo: 'https://srportfolio.blob.core.windows.net/portfolio-images/sarah2.jpg'
+  }
 }
 
 export default imageUrls;
