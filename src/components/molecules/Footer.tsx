ffeeae806728781b4b8c63352cf29827ad09910e
Sarah Reimann <reimannsarah45@gmail.com>
2023-12-11 23:03:34 -0800

    add links

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
     <div className='footer'>
       <p>{pageStrings.copyright}</p>
       <div className='logo-icons'>
-        <img src={imageUrls.links.github} alt="GitHub logo" />
-        <img src={imageUrls.links.linkedin} alt="LinkedIn logo" />
+        <a href='https://github.com/reimannsarah' target='blank'><img src={imageUrls.links.github} alt="GitHub logo" /></a>
+        <a href='https://www.linkedin.com/in/sarah-reimann/' target='blank'><img src={imageUrls.links.linkedin} alt="LinkedIn logo" /></a>
       </div>
         <img src={imageUrls.logos.logo_name} alt="logo"/>
     </div>
